2d7d5f8bcf01d9722462ed812c3a7897c285d06e
shanmugapriya247 <shanmugapriya2476@gmail.com>
2026-06-27 11:29:02 +0530

    Add files via upload

--- TwoPointerPalindrome.java
+++ TwoPointerPalindrome.java
@@ -1,3 +1,24 @@
+//without 2 pointer
+public class Palindrome {
+    public static void main(String[] args) {
+
+        String str = "madam";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        if (str.equals(rev)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
+
+
+//with 2 pointer
 public class Palindrome {
 
     public static void main(String[] args) {
