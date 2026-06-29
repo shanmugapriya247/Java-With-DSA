ed54577842809ea5e269ae6f2fdbbc2119004c62
shanmugapriya247 <shanmugapriya2476@gmail.com>
2026-06-29 12:20:38 +0530

    Add files via upload

--- ArrayListEmployeePrint.java
+++ ArrayListEmployeePrint.java
@@ -1,5 +1,6 @@
 //using for each loop
 
+
 import java.util.ArrayList;
 
 class Employee {
@@ -80,5 +81,66 @@ public class Main {
             System.out.println("Designation : " + emp.designation);
             System.out.println();
         }
+    }
+}
+
+
+//using for each loop
+
+import java.util.ArrayList;
+
+class Employee {
+
+    int empid;
+    String name;
+    String designation;
+    
+    Employee(int empid, String name, String designation)
+    {
+        this.empid = empid;
+        this.name = name;
+        this.designation = designation;
+    }
+    
+    void display()
+    {
+        System.out.println(empid);
+        System.out.println(name);
+        System.out.println(designation);
+    }
+}
+
+
+
+
+class Main{
+public static void main (String[] args) 
+    {
+       ArrayList <Employee> emp = new ArrayList<>();
+       
+       /*empList.add(new Employee(101, "Arun", "Software Engineer"));
+        empList.add(new Employee(102, "Priya", "Tester"));
+        empList.add(new Employee(103, "Rahul", "Manager"));*/
+        
+       Employee e = new Employee(1, "Shan", "hr");
+       Employee e1 = new Employee(2, "Thulasi", "CEO");
+       
+       System.out.println("Employee details");
+       emp.add(e);
+       emp.add(e1);
+       for(Employee a : emp)
+       {
+           
+           a.display();
+           /*System.out.println(a.empid);
+           System.out.println(a.name);
+           System.out.println(a.designation);*/
+           
+        /*empList.add(new Employee(101, "Arun", "Software Engineer"));
+        empList.add(new Employee(102, "Priya", "Tester"));
+        empList.add(new Employee(103, "Rahul", "Manager"));*/
+       }
+       
+       
     }
 }
