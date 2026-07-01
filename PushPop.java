e391e6e46202f19402b4e62e6c332978b4ca37d9
shanmugapriya247 <shanmugapriya2476@gmail.com>
2026-07-01 09:29:07 +0530

    Update PushPop.java

--- PushPop.java
+++ PushPop.java
@@ -33,3 +33,33 @@ public class PushPop
 		}
 	}
 }
+
+/*
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int n = 5;
+		int pushed[] = {1,2,3,4,5};
+		int poped[] = {4,5,3,2,1};
+		int index = 0;
+		Stack <Integer> s = new Stack<>();
+		for (int i=0; i<n; i++)
+		{
+		    s.push(pushed[i]);
+		    while(!s.isEmpty() && s.peek() == poped[index])
+		    {
+		        s.pop();
+		        index ++;
+		    }
+		}
+		if(s.isEmpty())
+		{
+		    System.out.print("True");
+		}
+		else{
+		    System.out.print("False");
+		}
+	}
+}
+*/
