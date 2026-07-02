f43456b70d94a0d0a9e82ca2afdd68d9898a65bb
shanmugapriya247 <shanmugapriya2476@gmail.com>
2026-07-02 10:41:26 +0530

    Add files via upload

--- UnionIntersection.java
+++ UnionIntersection.java
@@ -79,3 +79,91 @@ public class Main {
     }
 }
 */
+
+
+/*
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of first array: ");
+        int n1 = sc.nextInt();
+
+        int[] a = new int[n1];
+
+        System.out.println("Enter first array elements:");
+        for (int i = 0; i < n1; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter size of second array: ");
+        int n2 = sc.nextInt();
+
+        int[] b = new int[n2];
+
+        System.out.println("Enter second array elements:");
+        for (int i = 0; i < n2; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < n1; i++) {
+            set.add(a[i]);
+        }
+
+        for (int i = 0; i < n2; i++) {
+            set.add(b[i]);
+        }
+
+        System.out.println("Union = " + set);
+    }
+}
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of first array: ");
+        int n1 = sc.nextInt();
+
+        int[] a = new int[n1];
+
+        System.out.println("Enter first array elements:");
+        for (int i = 0; i < n1; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter size of second array: ");
+        int n2 = sc.nextInt();
+
+        int[] b = new int[n2];
+
+        System.out.println("Enter second array elements:");
+        for (int i = 0; i < n2; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < n1; i++) {
+            set.add(a[i]);
+        }
+
+        System.out.print("Intersection = ");
+
+        for (int i = 0; i < n2; i++) {
+
+            if (set.contains(b[i])) {
+                System.out.print(b[i] + " ");
+            }
+        }
+    }
+}
+*/
