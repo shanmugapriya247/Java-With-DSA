557973f86d82c27f0ad6c61cf71bc02cc1b0e97a
shanmugapriya247 <shanmugapriya2476@gmail.com>
2026-07-02 10:30:02 +0530

    Add files via upload

--- UnionIntersection.java
+++ UnionIntersection.java
@@ -29,3 +29,53 @@ public class UnionIntersection {
         System.out.println("Intersection: " + intersection);
     }
 }
+
+/*
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] a = {1, 2, 3, 4};
+        int[] b = {3, 4, 5, 6};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i : a) {
+            set.add(i);
+        }
+
+        for (int i : b) {
+            set.add(i);
+        }
+
+        System.out.println("Union: " + set);
+    }
+}
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] a = {1, 2, 3, 4};
+        int[] b = {3, 4, 5, 6};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        // Store first array
+        for (int i : a) {
+            set.add(i);
+        }
+
+        System.out.print("Intersection: ");
+
+        // Check second array
+        for (int i : b) {
+
+            if (set.contains(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+*/
