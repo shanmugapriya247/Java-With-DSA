3923579a6d4accc12670edc065262d511057e5f5
shanmugapriya247 <shanmugapriya2476@gmail.com>
2026-07-06 12:23:16 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,24 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] arr = {50, 25, 30, 70, 10, 12};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ SelectionSort.java
@@ -0,0 +1,28 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        int[] arr = {50, 25, 30, 70, 10, 12};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int min = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+
+        System.out.println("Sorted Array:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
